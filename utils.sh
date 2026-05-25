30827e9f69b27ebb845294368cb14498a5b63b40
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-26 01:42:25 +0700

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -475,7 +475,7 @@ build_rv() {
 	local tried_dl=()
 	for dl_p in archive apkmirror uptodown; do
 		if [ -z "${args[${dl_p}_dlurl]}" ]; then continue; fi
-		if ! get_${dl_p}_resp "${args[${dl_p}_dlurl]}" || ! pkg_name=$(get_"${dl_p}"_pkg_name); then
+		if ! get_${dl_p}_resp "${args[${dl_p}_dlurl]}"); then
 			args[${dl_p}_dlurl]=""
 			epr "ERROR: Could not find ${table} in ${dl_p}"
 			continue
